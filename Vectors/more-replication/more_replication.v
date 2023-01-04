430ef0b6e2bea4fe362a65a44d6eb60cc72fb8b7
Sean Berrios <berrios96sean@gmail.com>
2023-01-04 12:58:03 -0700

    modified:   more-replication/more_replication.v

--- Vectors/more-replication/more_replication.v
+++ Vectors/more-replication/more_replication.v
@@ -8,3 +8,52 @@ module more_replication (
                
 
 endmodule
+
+module testbench;
+    
+    // Inputs
+    reg a,b,c,d,e;
+
+    // Outputs
+    wire [24:0] out;
+    
+    // Instantiate the module
+    more_replication dut (
+    .a(a),
+    .b(b),
+    .c(c),
+    .d(d),
+    .e(e),
+    .out(out)
+    );
+    
+    initial begin
+        $dumpfile("more_replication.vcd");
+        $dumpvars(0, testbench);
+    end
+
+    initial begin
+
+        $display("Generating 8 random inputs");
+        // Generate 8 random inputs and print the inputs and outputs
+        for (integer i = 0; i < 8; i++) begin
+            a = $random;
+            b = $random;
+            c = $random;
+            d = $random;
+            e = $random;
+            #1;  // Wait 1 time unit
+            $display("Input a = %h", a);
+            $display("Input b = %h", b);
+            $display("Input c = %h", c);
+            $display("Input d = %h", d);
+            $display("Input e = %h", e);
+            $display("Output");
+            $display("Output = %h", out);
+            $display("\n");
+        end
+        
+        $finish;  // End the simulation
+    end
+    
+endmodule
