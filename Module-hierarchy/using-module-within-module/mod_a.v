32eb0d635d924ae93f754bec29c0283055813a01
Sean Berrios <berrios96sean@gmail.com>
2023-01-05 13:04:51 -0700

    new file:   mod_a.v 	new file:   top_module.v 	new file:   top_module.vcd 	new file:   top_module.vvp

---
+++ Module-hierarchy/using-module-within-module/mod_a.v
@@ -0,0 +1,6 @@
+module mod_a ( input in1, input in2, output out );
+    // Module body
+    // Since this is an example we will use and OR gate 
+    assign out = in1 | in2;
+
+endmodule
---
+++ Module-hierarchy/using-module-within-module/top_module.v
@@ -0,0 +1,79 @@
+`include "mod_a.v"
+
+module top_module ( input a, input b, output out );
+
+    mod_a instance1 ( .in1(a), .in2(b), .out(out) ); 
+
+endmodule
+
+module testbench;
+    
+    // Inputs
+    reg a,b;
+
+    // Outputs
+    wire out;
+    
+    // Instantiate the module
+    top_module dut (
+    .a(a),
+    .b(b),
+    .out(out)
+    );
+    
+    initial begin
+        $dumpfile("top_module.vcd");
+        $dumpvars(0, testbench);
+    end
+
+    initial begin
+
+
+        
+        #1;
+        a = 1'b0;
+        b = 1'b0; 
+        #5;
+        $display("Inputs");
+        $display("a = %b",a);
+        $display("b = %b",b);
+        $display("Output");
+        $display("Out = %b",out);
+        $display();
+
+        a = 1'b0;
+        b = 1'b1; 
+        #5;
+        $display("Inputs");
+        $display("a = %b",a);
+        $display("b = %b",b);
+        $display("Output");
+        $display("Out = %b",out);
+        $display();
+
+        a = 1'b1;
+        b = 1'b0; 
+        #5;
+        $display("Inputs");
+        $display("a = %b",a);
+        $display("b = %b",b);
+        $display("Output");
+        $display("Out = %b",out);
+        $display();
+
+        a = 1'b1;
+        b = 1'b1; 
+        #5;
+        $display("Inputs");
+        $display("a = %b",a);
+        $display("b = %b",b);
+        $display("Output");
+        $display("Out = %b",out);
+        $display();
+
+        
+        
+        $finish;  // End the simulation
+    end
+    
+endmodule
