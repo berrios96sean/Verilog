ff8b3d1c2e2899d0b4c9f7cd719108b3f3966bf1
Sean Berrios <42369838+berrios96sean@users.noreply.github.com>
2023-01-10 00:09:08 -0700

    Update top_module.v

--- Module-hierarchy/connect-ports-by-name/top_module.v
+++ Module-hierarchy/connect-ports-by-name/top_module.v
@@ -11,5 +11,8 @@ module top_module (
 
 endmodule
 
-// Since this code is repetitive and is the same as connect by posistion I will not 
-// Be writing a testbench for this solution 
+// Since this code is repetitive and is essentially the same as connect by posistion I will not 
+// Be writing a testbench for this solution. This solution can be tested on the HDL bits website 
+// which will generate the correct solution and waveform. It should be noted that the main difference 
+// between the two is that in this solution the calls had to be made to the correct I/O in the mod_a 
+// module. (ex. .out1(out1)) assigning by posistion will not work for this soltion. 
